11253238760fb49df25ceb15b50901ce6bbe184a
krishnamurthykarri1999 <119151203+krishnamurthykarri1999@users.noreply.github.com>
2025-10-05 11:47:59 +0530

    Update ExceptionHandling.java

--- src/main/java/com/getJob/Optional/ExceptionHandling.java
+++ src/main/java/com/getJob/Optional/ExceptionHandling.java
@@ -1,14 +1,20 @@
 package com.getJob.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExceptionHandling {
 
-	@GetMapping("/access")
-	public String  get() {
-		return "access krishnamurthy";
-	}
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandling.class);
 
+    @GetMapping("/test")
+    public String test() {
+        logger.info("Info lsog: /test API called");
+        logger.error("Error log example");
+        return "Logging test successful!";
+    }
 }
+
